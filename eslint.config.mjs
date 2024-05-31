848759253fddcfa64cf7cf6c65053b0e172d97c4
Mattkloz <mjklosinski@gmail.com>
2024-05-31 15:49:19 -0400

    cleanup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,5 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
     files: ['**/*.ts', '**/*.tsx'],
-    rules: {
-        'no-console': 'off', // allow console.log in TypeScript files
-    },
+    rules: {},
 })
